41fbfff3232f886bd654a0eb408bd5454ebe8e42
Gowrisankar24 <sankarsri2410@gmail.com>
2024-11-30 20:13:03 +0530

    Intial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    eslint: {
+        ignoreDuringBuilds: false,
+    },
 };
 
 export default nextConfig;
